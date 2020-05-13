154e95b2a2fc35834919d8cd593abdebb5ae059e
Arun kumar <59803550+developer-arun@users.noreply.github.com>
2020-05-13 21:50:29 +0530

    commit

--- QR_scanner/src/main/java/com/example/qrscanner/PopupWindow_ok.java
+++ QR_scanner/src/main/java/com/example/qrscanner/PopupWindow_ok.java
@@ -7,6 +7,15 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PopupWindow_ok extends AppCompatActivity {
     public static TextView qr_text;
@@ -23,8 +32,31 @@ public class PopupWindow_ok extends AppCompatActivity {
         imbtn_confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                    String Url = "url";
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, Url, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
 
-            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    }
+                    ) {
+                        protected Map<String, String> getParams() {
+                            Map<String, String> parr = new HashMap<String, String>();
+                            parr.put("data",ORscanner_code.qr_data );
+                            return parr;
+                        }
+
+                    };
+                    AppController.getInstance().addToRequestQueue(stringRequest);
+
+                    Toast.makeText(getApplicationContext(), "uploaded Successfully!", Toast.LENGTH_LONG).show();
+                }
+
         });
         DisplayMetrics displayMetrics=new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
