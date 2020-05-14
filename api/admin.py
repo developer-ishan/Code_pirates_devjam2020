62a5bee43f129aa305e01f957d44eacbd07195a0
lovedeep <developer.lovedeeep@gmail.com>
2020-05-15 01:41:44 +0530

    changed all html files to template version

--- api/admin.py
+++ api/admin.py
@@ -3,7 +3,7 @@ from .models import entry_status
 
 
 class entry_status_Admin(admin.ModelAdmin):
-    #todo: find on internet and change hostel to first_name
+    #todo: find on internet and change user to first_name
     list_display = ('user', 'show_tick','is_opening_entry')
 
 admin.site.register(entry_status,entry_status_Admin)
--- user/models.py
+++ user/models.py
@@ -8,11 +8,9 @@ from communities.models import community
 class user_profile(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     regno = models.PositiveIntegerField(primary_key=True, validators=[MinValueValidator(11111111,'please enter valid registration no'),MaxValueValidator(99999999,'please enter valid registration no')])
-    # todo: make this a select from dropdown 
     branch = models.CharField(max_length=3)
     group = models.CharField(max_length=2)
     roomno = models.PositiveIntegerField(validators=[MinValueValidator(100,'please enter valid room no'),MaxValueValidator(999,'please enter valid room no')])
-    #todo: make this a select field from dropdown
     hostel = models.CharField(max_length=255)
     profile_pic = models.ImageField(upload_to='static/images/profile_pic',default = 'static/images/profile_pic/avatar.png')
     following = models.ManyToManyField(community,blank = True)
